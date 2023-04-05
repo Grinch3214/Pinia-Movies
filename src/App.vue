f36efa65df76dd875ebf20cbca3ad1173184ec29
Max <scorpiomax90@gmail.com>
2023-04-05 13:15:12 +0300

    added state and html markup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
-	<header class="header">
-		<h2>Movies</h2>
-	</header>
-	<div v-for="item in movieStore.movies" :key="item.id">
-		{{ item.original_title }}
-	</div>
+	<main>
+		<header class="header">
+			<img class="header-logo" src="/logo.svg" alt="">
+			<h2>Movies (with Pinia)</h2>
+		</header>
+		<div class="tabs">
+			<button :class="['btn', {btn_green: movieStore.activeTab === 1}]">Favorite</button>
+			<button :class="['btn', {btn_green: movieStore.activeTab === 2}]">Search</button>
+		</div>
+		<div class="movies" v-if="movieStore.activeTab === 1">
+			<h3>All Movies</h3>
+			<Movie
+				v-for="movie in movieStore.movies"
+				:key="movie.id"
+				:movie="movie"
+			/>
+		</div>
+		<div class="search" v-else>Search</div>
+	</main>
 </template>
 
 <script setup>
+import Movie from './components/Movie.vue'
 import { useMovieStore } from './stores/MovieStore';
 
 const movieStore = useMovieStore();
 </script>
 
-<style>
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
 </style>
